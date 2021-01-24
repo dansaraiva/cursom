c8b5293730cafec6836147e7f8f64b583a5bc3b0
Dan Saraiva <saraivadaniel@protonmail.com>
2021-01-23 21:54:14 -0300

    Criando repository e service para Catgorias

---
+++ src/main/java/com/dansaraiva/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.dansaraiva.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.dansaraiva.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria,Integer> {
+
+}
